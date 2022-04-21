334fcbeb38916c64b9c2903faff431c271ac64e5
CWJWANJING <wanjingchencwj@gmail.com>
2022-04-21 21:56:31 +0100

    Add end-to-end diary submission
    Add template with diary entry box and submit button
    Serve template and handle post submission
    Add database model for diary

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
+# tell sqlalchemy where the sql database is
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+# sqlalchemy will manage the schema in the code
+db = SQLAlchemy(app)
 
-@app.route("/")
+class Diary(db.Model):
+    day = db.Column(db.Date, primary_key=True)
+    entry = db.Column(db.String)
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return "hello, world"
+    if request.method == "POST":
+        d = Diary(day=date.today(), entry=request.form["entry"])
+        db.session.add(d)
+        db.session.commit()
+    return render_template('today.html')
+
+
 
 if __name__ == "__main__":
     app.run()
